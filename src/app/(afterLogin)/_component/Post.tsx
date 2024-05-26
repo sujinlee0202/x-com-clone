e21f592c90718c6efdb70c40a5708d9cc7948e99
titch <sujinlee0202@gmail.com>
2024-05-26 17:52:33 +0900

    PostForm, Post 컴포넌트 생성
    - dayjs 라이브러리 추가
    - classnames 라이브러리 추가
    - Home - Post, PostForm UI 추가

--- src/app/(afterLogin)/_component/Post.tsx
+++ src/app/(afterLogin)/_component/Post.tsx
@@ -1,6 +1,60 @@
-import Image from "next/image";
 import React from "react";
+import styles from "./Post.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import "dayjs/locale/ko";
+import ActionButtons from "./ActionButtons";
+
+dayjs.extend(relativeTime);
+dayjs.locale("ko");
 
 export default function Post() {
-  return <div>Post</div>;
+  const target = {
+    User: {
+      id: "elonmusk",
+      nickname: "Elon Musk",
+      image: "/zlogo.png",
+    },
+    content: "클론코딩 라이브로 하니 너무 힘들어요 ㅠㅠ",
+    createdAt: new Date(),
+    Images: [],
+  };
+
+  return (
+    <article className={styles.post}>
+      <div className={styles.postWrapper}>
+        <div className={styles.postUserSection}>
+          <Link href={`/${target.User.id}`} className={styles.postUserImage}>
+            <Image
+              src={target.User.image}
+              alt={target.User.nickname}
+              width={10}
+              height={10}
+            />
+            <div className={styles.postShade} />
+          </Link>
+        </div>
+        <div className={styles.postBody}>
+          <div className={styles.postMeta}>
+            <Link href={`/${target.User.id}`}>
+              <span className={styles.postUserName}>
+                {target.User.nickname}
+              </span>
+              &nbsp;
+              <span className={styles.postUserId}>@{target.User.id}</span>
+              &nbsp; · &nbsp;
+            </Link>
+            <span className={styles.postDate}>
+              {dayjs(target.createdAt).fromNow(true)}
+            </span>
+          </div>
+          <div>{target.content}</div>
+          <div className={styles.postImageSection}></div>
+          <ActionButtons />
+        </div>
+      </div>
+    </article>
+  );
 }
